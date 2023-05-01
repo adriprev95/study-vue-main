12eb1c5b0a0466e9765788eb715477233f8e182c
Demoad <adriprev95@gmail.com>
2023-05-02 01:45:23 +0200

    Arreglando la view photo

--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>culo</div>
+    <div class="title w-screen md:mx-auto grid grid-rows-2 grid-flow-col gap-2 justify-center py-5">
+        <h1 class="text-2xl">Pagina de Usuarios</h1>
+        <h2>#: {{ people.length }} algo</h2>
+    </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
