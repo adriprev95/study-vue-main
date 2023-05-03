8bfe3bf3c7e6c0e5cd88f9d2fab7b74935b6302c
Demoad <adriprev95@gmail.com>
2023-05-03 22:21:35 +0200

    Arreglos a la view de photos

--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -351,16 +351,57 @@ let photos = ref([
         "url": "https://via.placeholder.com/600/9e59da",
         "thumbnailUrl": "https://via.placeholder.com/150/9e59da"
     },
-])
+]);
+
+let s = "s"
 </script>
 
 <template>
     <div class="title w-screen md:mx-auto grid grid-rows-2 grid-flow-col gap-2 justify-center py-5">
         <h1 class="text-2xl">Pagina photos</h1>
-        <h2>#: {{ photos.length }} algo</h2>
+        <h2>#: {{ photos.length }}</h2>
+    </div>
+    <div>
+        <div class=" flex flex-wrap justify-center connte mx-5">
+            <div v-for="photo in photos" :key="photo.id">
+                <div v-if="photo.id != 43">
+                    <div v-if="!photo.title.endsWith(s)">
+                        <div v-if="photo.id % 2 == 0">
+                            <div class="flex flex-wrap md:w-80 mx-5">
+                                <img :src="photo.url" alt="" class=" photo">
+                                <div class="flex flex-wrap justify-center content-around mx-2">
+                                    <span class=" text-justify ">{{ photo.title }}</span>
+                                    <span class=" text-justify ">{{ photo.id }}</span>
+                                    <span class=" text-justify "> el id es par</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div v-else>
+                            <div class="flex flex-wrap md:w-80 mx-5">
+                                <img :src="photo.url" alt="" class=" photo">
+                                <div class="flex flex-wrap justify-center content-around mx-2">
+                                    <span class=" text-justify ">{{ photo.title }}</span>
+                                    <span class=" text-justify ">{{ photo.id }}</span>
+                                    <span class=" text-justify "> el id es impar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 
 
-<style scoped></style>
+<style scoped>
+.title {
+    background-color: aliceblue;
+    height: 100px;
+}
+
+.algo {
+    width: 250px;
+}
+</style>
